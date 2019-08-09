658047c3c7ac8e69b3640c97f5461f6baa2084a1
Seok Kwun Park <park960@purdue.edu>
2019-08-08 22:30:41 -0400

    initial commit

---
+++ Restaurant_Server_Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant_Server_Client
+//
+//  Created by chris on 8/8/19.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
